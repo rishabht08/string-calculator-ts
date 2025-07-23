8c9bfb0fcc10af20620a3d41013b4443b079afcf
rishabht08 <rishabht123@gmail.com>
2025-07-23 23:35:07 +0530

    Utility method testing

---
+++ tests/parser.test.ts
@@ -0,0 +1,32 @@
+import { parseDelimiter } from "../src/parser/delimiterParser";
+import { parseNumbers } from "../src/parser/parseNumbers";
+
+describe('String Calculator Parser Utility methods', () => {
+    it('returns default delimiter for normal input', () => {
+        const result = parseDelimiter('1,2\n3');
+        expect(result).toEqual({
+            delimiter: ',|\n',
+            numbersString: '1,2\n3'
+        });
+    });
+
+    it('parses custom delimiter', () => {
+        const result = parseDelimiter('//;\n5;8');
+        expect(result).toEqual({
+            delimiter: ';',
+            numbersString: '5;8'
+        });
+    });
+
+    it('parses comma-delimited numbers', () => {
+        expect(parseNumbers('1,2,3', ',')).toEqual([1, 2, 3]);
+    });
+
+    it('parses newline-delimited numbers', () => {
+        expect(parseNumbers('4\n5\n6', '\n')).toEqual([4, 5, 6]);
+    });
+
+     it('parses custom-delimited numbers', () => {
+        expect(parseNumbers('4;5;6', ';')).toEqual([4, 5, 6]);
+    });
+});
